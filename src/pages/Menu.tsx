bd5f470faa443d2326b3933bb12f262ffed9eda1
nober <nobertorafael9@hotmail.com>
2025-12-14 01:47:34 -0300

    teste

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -18,6 +18,7 @@ export default function Menu() {
     : items;
 
   useEffect(() => {
+    alert(API_BASE_URL)
     console.log(API_BASE_URL)
     debugger
     const claim = quiosqueStorage.getClaim();
